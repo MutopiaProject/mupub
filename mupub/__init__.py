8796f1fc6d941d9f74cb86e54e2a65f4e2be504e
Glen Larsen <glenl.glx@gmail.com>
2017-11-04 14:53:25 -0700

    bumped to 1.0.0-beta

--- mupub/__init__.py
+++ mupub/__init__.py
@@ -17,7 +17,7 @@ During development of an upcoming release, the MINOR number may be
 incremented.
 
 """
-__version__ = '0.7.0'
+__version__ = '1.0.0-beta'
 
 __author__ = 'Glen Larsen and Mutopia Project contributors'
 __author_email__= 'glenl.glx@gmail.com'
